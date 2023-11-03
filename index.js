1a876190058f520ade618db422ca0ccfb3db108f
EthanDotLog <148747739+EthanDotLog@users.noreply.github.com>
2023-11-03 18:30:27 -0500

    added rounds span to html
    rounds now counted
    reset functional
    comments on js code added

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const pcWinsEl = document.querySelector("#pc-wins-el");
 const roundResponseEl = document.querySelector("#round-response-el");
 const noLivesMessageEl = document.querySelector("#no-lives-message-el");
 const resetBtn = document.querySelector("#reset-btn");
+const roundCount = document.querySelector("#round-count")
 
 
 
@@ -13,39 +14,42 @@ const resetBtn = document.querySelector("#reset-btn");
 let userChoice;
 let userWins = 0;
 let pcWins = 0;
+let round = 0;
 
 
 
 
 //buttons
-
+//user submit checks user input for eligibility - clears the input field - starts the game
 userSubmitBtn.addEventListener('click', function() {
     const playerChoice = userInputEl.value;
     if (playerChoice === 'rock' | playerChoice === 'paper' | playerChoice === 'scissors') {
         userChoice = playerChoice
+        clearinput()
         game()
+    } else {
+        roundResponseEl.textContent = `I am sorry, ${userInputEl.value} is not an allowed choice`
+        clearinput()
     }
 });
 
+//rests the game values and txt fields via a function
 resetBtn.addEventListener('click', function() {
-    userWinsEl.textContent = 0;
-    pcWinsEl.textContent = 0;
-});
+    reset()
+})
 
-
-
-
-
+//functions
+//checks if you are under 5 losses, adds 1 to the round counter, prints - runs play round
 function game() {
     if (pcWins >= 5) {
-        noLivesMessageEl.textContent = "You cant play if you have already lost 5 rounds Reset and try again"
+        noLivesMessageEl.textContent = "You cant play if you have already lost 5 rounds - Reset and try again"
     } else {
+        ++round
         printResults(playRound(userChoice, getComputerChoice()))
     }
 };
 
-
-
+//gets the computer choice using a random number generator that gives 1-3 as results
 function getComputerChoice() {
     const randomNum = Math.floor((Math.random() * 3) +1)
 
@@ -58,9 +62,7 @@ function getComputerChoice() {
     }
 }
 
-
-
-
+//puts 2 variables against each other in rock paper scissors for 1 round, adds wins and losses, and returns a statement 
 function playRound(playerChoice, computerChoice) {
     const player = playerChoice.toLowerCase();
     const pc = computerChoice.toLowerCase();
@@ -96,11 +98,27 @@ function playRound(playerChoice, computerChoice) {
             return `You Tied - ${player} can't beat ${pc}`
         };
     };
-    userInputEl.value = ""
 };
 
+//prints the results, requires a input for the message el
 function printResults(message) {
     userWinsEl.textContent = userWins
     pcWinsEl.textContent = pcWins
     roundResponseEl.textContent = message
+    roundCount.textContent = round
 };
+
+//clears the user input field only
+function clearinput() {
+    userInputEl.textContent = "";
+    userInputEl.value = "";
+}
+
+//resets everything
+function reset() {
+    userWins = 0
+    pcWins = 0
+    round = 0
+    message = ""
+    printResults(message)
+}
